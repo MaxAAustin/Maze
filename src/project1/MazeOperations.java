e6e0959649993fa34487ac6a080ddcd3cae05b16
michael_austin <maxaustin92@gmail.com>
2017-09-18 13:44:24 -0400

    First draft of MazeOperations finished.
    Too tired to perform unit tests.

--- src/project1/MazeOperations.java
+++ src/project1/MazeOperations.java
@@ -33,31 +33,46 @@ public class MazeOperations {
         final int MBCLow = 0, MBRLow = 0;
         final int MBCHigh = 14, MBRHigh = 14;
         // final variable to use for direction changes and traversing maze
-        final int wall = 0;
-        final int emptyPath = 1;
-        final int visited = 3;
-        final int backtrack = 7;
+
+        int up = theMaze[row+1][col];
+        int down = theMaze[row-1][col];
+        int right = theMaze[row][col+1];
+        int left = theMaze[row][col-1];
 
         // Tests whether we are at the end of the maze. If yes, return true. If not, keep going.
-        if(traverseMaze(theMaze, row, col) == traverseMaze(theMaze, 14, 14))
-            return true;
+        if ((row < MBRLow | row > MBRHigh) | (col < MBCLow | col > MBCHigh))return false;
         else{
-            if (theMaze[row][col]== 0){
-                traverseMaze(theMaze, row, col - 1);
+            if (traverseMaze(theMaze, row, col) == traverseMaze(theMaze, 14, 14))
+                return true;
+            else {
+                if (theMaze[row][col] == 0) {
+                    traverseMaze(theMaze, row, col - 1);
+                    return false;
+                }
+                //Go up
+                else if (up == 1 || up == 7) {
+                    traverseMaze(theMaze, row, col + 1);
+                    return false;
+                }
+                //Go down
+                else if (down == 1 || down == 7) {
+                    traverseMaze(theMaze, row - 1, col);
+                    return false;
+                }
+                //Go right
+                else if (right == 1 || right == 7) {
+                    traverseMaze(theMaze, row, col + 1);
+                    return false;
+                }
+                //Go left
+                else if (left == 1 || left == 7) {
+                    traverseMaze(theMaze, row, col - 1);
+                    return false;
+                }
+                /* Tests to see if next cell has been visited, column and row of next cell are within bounds, and next cell
+                is not a wall. */
                 return false;
             }
-            else if(theMaze[row][col] == 1){
-                theMaze[row][col] = 3;
-                traverseMaze(theMaze, row, col+1);
-                return false;
-            }
-            /* Tests to see if next cell has been visited, column and row of next cell are within bounds, and next cell
-            is not a wall. */
-            else if(theMaze[row][col] == 3){
-                theMaze[row][col] = 7;
-                traverseMaze(theMaze, row, col-1);
-            }
-            return false;
         }
     }
 
