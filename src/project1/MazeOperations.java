d88afd8397cced75157d0b28750f2e74188f9fb8
Michael Austin <maxaustin92@gmail.com>
2017-09-18 07:50:41 -0400

    Desktop branch working on directional changes in traverse method. Fleshed out print and fill maze methods

--- src/project1/MazeOperations.java
+++ src/project1/MazeOperations.java
@@ -15,28 +15,14 @@ public class MazeOperations {
     public static void fillMaze(int[][] theMaze, Scanner inputFile){
         for(int r = 0; r<= 14; r++){
             for(int c = 0; c<=r; c++){
-
+                theMaze[r][c] = inputFile.nextInt();
+                fillMaze(theMaze, inputFile);
             }
         }
     }
 
     public static void printMaze(int[][] theMaze){
-
-    }
-
-    public static boolean notAWall(int value) {
-        boolean visited;
-
-        if (value == 0)
-            return false;
-        else if (value == 1) {
-            value = 7;
-            visited = true;
-            return true;
-        } else {
-            visited = true;
-            return true;
-        }
+        System.out.println(theMaze);
 
     }
 
@@ -48,22 +34,24 @@ public class MazeOperations {
         final int MBCHigh = 14, MBRHigh = 14;
 
         // Tests whether we are at the end of the maze. If yes, return true. If not, keep going.
-        if(notAWall() & (col <= MBCHigh & col >= MBCLow) & (row <= MBRHigh & row >= MBRLow) &
-                (notAWall)){
-            if(traverseMaze(theMaze, row, col) == traverseMaze(theMaze, 14, 14))
-                return true;
-            else{
+        if(traverseMaze(theMaze, row, col) == traverseMaze(theMaze, 14, 14))
+            return true;
+        else if((theMaze[row][col] == 1) & (col <= MBCHigh & col >= MBCLow) & (row <= MBRHigh & row >= MBRLow)){
+            theMaze[row][col] = 3;
+            traverseMaze(theMaze, row, col+1);
+            return false;}
+
+        else if (theMaze[row][col]== 0){
                 theMaze[row][col] = 7;
+                traverseMaze(theMaze, row, col + 1);
+                return false;
             }
         }
 
-        else{
+        else if((theMaze[row][col] == 3) & (col <= MBCHigh & col >= MBCLow) & (row <= MBRHigh & row >= MBRLow)){
             /* Tests to see if next cell has been visited, column and row of next cell are within bounds, and next cell
             is not a wall. */
-            if((NotVisited == true) & (col <= MBCHigh | col >= MBCLow) & (row <= MBRHigh | row >= MBRLow) &
-                    (notAWall==true)){
-                if ()
-                    traverseMaze(theMaze,row+1,col);
+            if(){
             }
         }
     }
